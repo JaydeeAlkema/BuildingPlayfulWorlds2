416f09cc189d1056d266d8c98dbbedbeb92c7fa1
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-14 18:08:24 +0200

    Add Functionality

--- Assets/Scripts/MazeDoor.cs
+++ Assets/Scripts/MazeDoor.cs
@@ -3,4 +3,18 @@
 public class MazeDoor : MazePassage
 {
 	public Transform hinge = default;
+
+	private MazeDoor GetOtherSideOfDoor() => otherCell.GetEdge(direction.GetOpposite()) as MazeDoor;
+
+	public override void Initialize(MazeCell primary, MazeCell other, MazeDirection direction)
+	{
+		base.Initialize(primary, other, direction);
+		if(GetOtherSideOfDoor() != null)
+		{
+			hinge.localScale = new Vector3(-1f, 1f, 1f);
+			Vector3 p = hinge.localPosition;
+			p.x = -p.x;
+			hinge.localPosition = p;
+		}
+	}
 }

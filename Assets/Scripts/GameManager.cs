3dda2a3cd1b24112458ea8365e896d468f86932f
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-14 20:33:41 +0200

    Add Player spawning in the starting room

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,22 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 	#region Variables
 	[SerializeField] private Maze mazePrefab = default;
+	[SerializeField] private GameObject playerPrefab = default;
+	[SerializeField] private MazeCell cellToSpawnPlayerOn = default;
+
 	private Maze mazeInstance;
+	private GameObject playerInstance;
 	#endregion
 
 	#region Monobehaviour Callbacks
 	private void Start()
 	{
-		BeginGame();
+		StartCoroutine(BeginGame());
 	}
 
 	private void Update()
@@ -23,16 +29,22 @@ public class GameManager : MonoBehaviour
 	#endregion
 
 	#region Private Functions
-	private void BeginGame()
+	private IEnumerator BeginGame()
 	{
+		playerInstance = Instantiate(playerPrefab) as GameObject;
+		playerInstance.SetActive(false);
+
 		mazeInstance = Instantiate(mazePrefab) as Maze;
-		mazeInstance.Generate();
+		yield return StartCoroutine(mazeInstance.Generate());
+		cellToSpawnPlayerOn = mazeInstance.Rooms[0].Cells[Random.Range(0, mazeInstance.Rooms[0].Cells.Count)];
+
+		playerInstance.transform.position = new Vector3(cellToSpawnPlayerOn.transform.position.x, .25f, cellToSpawnPlayerOn.transform.position.z);
+		playerInstance.SetActive(true);
 	}
 
 	private void RestartGame()
 	{
-		Destroy(mazeInstance.gameObject);
-		BeginGame();
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 	}
 	#endregion
 }

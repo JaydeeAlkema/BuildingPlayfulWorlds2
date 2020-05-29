be7bf5968b3407cfb9edd4cbd79f2a700657e460
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-05-29 11:57:29 +0200

    Make class ready for ground based spell

--- Assets/Scripts/Spells/Spell.cs
+++ Assets/Scripts/Spells/Spell.cs
@@ -1,17 +1,39 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
 
+public enum SpellType
+{
+	TargetBased,
+	GroundBased
+}
+
+/// <summary>
+/// A spell class that handles... Well, being a spell.
+/// This really should be a base class where I inherit from. But lazyness got the best of me.
+/// </summary>
 public class Spell : MonoBehaviour
 {
 	#region Variables
+	[SerializeField] private SpellType spellType = SpellType.TargetBased;   // Type of the spell.
 	[SerializeField] private string spellName = default;                    // Name of the spell.
 	[SerializeField] private float manaCost = default;                      // How much mana it costs to cast this spell.
-	[SerializeField] private Transform target = default;                    // Where to move to.
-	[SerializeField] private float lerpTime = default;                     // How fast to move to target.
-	[SerializeField] private float damageToDeal = default;                  // How much damage to deal on impact.
-	[SerializeField] private float impactRadius = default;                  // The radius of impact. So how close the spell has to come to the target for impact.
-	[SerializeField] private GameObject impactParticles = default;          // Particle system to spawn on impact.
 
+	[Header("Base Variables")]
+	[SerializeField] private float damageToDeal = default;                  // How much damage to deal on impact.
+
+	[Header("Target Based Spell")]
+	[SerializeField] private Transform target = default;                    // Where to move to.
+	[SerializeField] private float lerpTime = default;                      // How fast to move to target.
+	[SerializeField] private GameObject impactParticles = default;          // Particle system to spawn on impact.
 	[SerializeField] private Vector3 startingPos = default;                 // the starting position of the enemy.
+
+	[Header("Ground Based Spell")]
+	[SerializeField] private float spellDuration = default;                 // How long the spell lasts untill it stops.
+	[SerializeField] private float damageInterval = default;                // How much time between damage ticks.
+	[SerializeField] private float impactRadius = default;                  // The radius of impact. So how close the spell has to come to the target for impact.
+	[SerializeField] private GameObject[] targetsInRadius = default;        // Array with all the targets within the radius.
+
 	private float timeStartedLerping;
 	private bool isActive = true;                                           // If the spell is active.
 	#endregion
@@ -29,7 +51,13 @@ public class Spell : MonoBehaviour
 
 	private void Update()
 	{
-		MoveToTarget();
+		if(spellType == SpellType.TargetBased) MoveToTarget();
+	}
+
+	private IEnumerator GroundedBasedDamageSpell()
+	{
+		// Do stuff
+		yield return null;
 	}
 
 	/// <summary>

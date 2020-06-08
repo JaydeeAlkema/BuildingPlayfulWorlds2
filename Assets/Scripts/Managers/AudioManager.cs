fa5631d08d56ebb130f8ca3604c5cdab6489064d
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-06-08 13:14:41 +0200

    Add Audio Manager

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+	#region Variables
+	private AudioManager instance = null;
+	#endregion
+
+	#region Monobehaviour
+	private void Awake()
+	{
+		if(!instance || instance != this) instance = this;
+	}
+	#endregion
+
+	#region Public Voids
+	/// <summary>
+	/// Plays a sound effect.
+	/// </summary>
+	/// <param name="clip">Which Audio Clip to play.</param>
+	/// <param name="pos">Where the audio will be played. (World Space)</param>
+	/// <param name="volume">How loud the clip should be.</param>
+	public void PlaySoundFX(AudioClip clip, Vector3 pos, float volume) => AudioSource.PlayClipAtPoint(clip, pos, volume);
+	#endregion
+}

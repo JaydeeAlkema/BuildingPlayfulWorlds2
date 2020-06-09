8f838329e42ad204917c8087c5a3d32fc44586d2
Jaydee Alkema <dev.jaydeealkema@gmail.com>
2020-06-09 17:40:41 +0200

    Replace every occurrence of the word "enemy" with "AI"

--- Assets/Scripts/AI/AIBehaviour.cs
+++ Assets/Scripts/AI/AIBehaviour.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public enum EnemyState
+public enum State
 {
 	Idle,
 	Chasing,
@@ -13,19 +13,19 @@ public enum EnemyState
 public class AIBehaviour : MonoBehaviour, IDamageable
 {
 	#region Variables
-	[SerializeField] private float health = default;                                // How much health the enemy has.
+	[SerializeField] private float health = default;                                // How much health the AI has.
 	[SerializeField] private float damage = default;                                // How much damage to deal to the player.
-	[SerializeField] private int manaWorth = default;                               // How much Mana the player gets back for killing an enemy.
+	[SerializeField] private int manaWorth = default;                               // How much Mana the player gets back for killing an AI.
 
 	[Header("Core Properties")]
-	[SerializeField] private EnemyState state = EnemyState.Idle;                    // State of the Enemy.
-	[SerializeField] private NavMeshAgent agent = default;                          // Reference to the NavMeshAgent component on the Enemy Gameobject.
+	[SerializeField] private State state = State.Idle;                    // State of the AI.
+	[SerializeField] private NavMeshAgent agent = default;                          // Reference to the NavMeshAgent component on the AI Gameobject.
 	[SerializeField] private Animator anim = default;                               // Reference to the animator component.
 	[SerializeField] private float onHitAudioVolume = 0.5f;                         // Volume of the on hit audio.
 	[SerializeField] private AudioClip[] onHitAudioClips = default;                 // Array with all the on hit sound effects.
 
 	[Header("Movement Properties")]
-	[SerializeField] private float moveSpeed = 3.5f;                                // Walking speed of the Enemy.
+	[SerializeField] private float moveSpeed = 3.5f;                                // Walking speed of the AI.
 	[SerializeField] private float movementAudioVolume = 0.1f;                      // Volume of the movement audio.
 	[SerializeField] private AudioSource audioSource = default;                     // Reference to the audio source component.
 	[SerializeField] private AudioClip[] walkSoundAudioClips = default;             // Array with all the walk sound effects.
@@ -36,15 +36,15 @@ public class AIBehaviour : MonoBehaviour, IDamageable
 	[SerializeField] private float targetDestinationUpdateInterval = 0.1f;          // How often the destination will be set for the AI.
 	[SerializeField] private float targetDetectionCheckInterval = 0.1f;             // How much time between target detection.
 	[SerializeField] private float targetInteractionInterval = 1f;                  // How much time between attacks.
-	[SerializeField] private float targetDetectionRadius = 15f;                     // How far the Enemy can "See".
-	[SerializeField] private float targetInteractionRadius = 1f;                    // How far the Enemy can "Interact" with the target.
+	[SerializeField] private float targetDetectionRadius = 15f;                     // How far the AI can "See".
+	[SerializeField] private float targetInteractionRadius = 1f;                    // How far the AI can "Interact" with the target.
 
 	private float maxMoveSpeed;
 	#endregion
 
 	#region Properties
 	public float MoveSpeed { get => moveSpeed; set => moveSpeed = value; }
-	public EnemyState State { get => state; set => state = value; }
+	public State State { get => state; set => state = value; }
 	#endregion
 
 	#region Monobehaviour Callbacks
@@ -59,10 +59,10 @@ public class AIBehaviour : MonoBehaviour, IDamageable
 
 	private void Update()
 	{
-		if(state != EnemyState.Dead)
+		if(state != State.Dead)
 		{
 			anim.SetFloat("Velocity", agent.velocity.magnitude);
-			anim.SetBool("Attacking", state == EnemyState.Attacking ? true : false);
+			anim.SetBool("Attacking", state == State.Attacking ? true : false);
 		}
 	}
 
@@ -78,14 +78,13 @@ public class AIBehaviour : MonoBehaviour, IDamageable
 	#region Functions
 	/// <summary>
 	/// Searches for the nearest target.
-	/// As of now it just gets the player from the Game Manager.
 	/// </summary>
 	/// <returns></returns>
 	private IEnumerator SearchForTarget()
 	{
 		while(true)
 		{
-			if(state == EnemyState.Idle)
+			if(state == State.Idle)
 			{
 				agent.isStopped = false;
 				Debug.Log("[" + gameObject.name + "]" + " Searching for Target");
@@ -103,28 +102,28 @@ public class AIBehaviour : MonoBehaviour, IDamageable
 					}
 				}
 				target = nearestCollider.transform;
-				state = EnemyState.Chasing;
+				state = State.Chasing;
 			}
 			yield return new WaitForSeconds(targetDetectionCheckInterval);
 		}
 	}
 
 	/// <summary>
-	/// Moves the enemy to the target at a set interval.
+	/// Moves the AI to the target at a set interval.
 	/// </summary>
 	/// <returns></returns>
 	private IEnumerator MoveToTarget()
 	{
 		while(true)
 		{
-			if(state == EnemyState.Chasing)
+			if(state == State.Chasing)
 			{
 				agent.isStopped = false;
 				agent.speed = moveSpeed;
 				agent.destination = target.position;
 				Debug.Log("[" + gameObject.name + "]" + " Moving Towards Target");
 
-				if(Vector3.Distance(transform.position, target.position) < targetInteractionRadius) state = EnemyState.Attacking;
+				if(Vector3.Distance(transform.position, target.position) < targetInteractionRadius) state = State.Attacking;
 			}
 			yield return new WaitForSeconds(targetDestinationUpdateInterval);
 		}
@@ -139,13 +138,13 @@ public class AIBehaviour : MonoBehaviour, IDamageable
 		while(true)
 		{
 			yield return new WaitForSeconds(targetInteractionInterval);
-			if(state == EnemyState.Attacking)
+			if(state == State.Attacking)
 			{
 				agent.isStopped = true;
 				target.GetComponent<IDamageable>()?.Damage(damage);
 
 				Debug.Log("[" + gameObject.name + "]" + " Attacking Target");
-				if(Vector3.Distance(transform.position, target.position) > targetInteractionRadius) state = EnemyState.Chasing;
+				if(Vector3.Distance(transform.position, target.position) > targetInteractionRadius) state = State.Chasing;
 			}
 			yield return null;
 		}
@@ -159,7 +158,7 @@ public class AIBehaviour : MonoBehaviour, IDamageable
 	{
 		while(true)
 		{
-			if(state == EnemyState.Chasing)
+			if(state == State.Chasing)
 			{
 				int randIndex = Random.Range(0, walkSoundAudioClips.Length);
 				audioSource.PlayOneShot(walkSoundAudioClips[randIndex], movementAudioVolume);
@@ -183,7 +182,7 @@ public class AIBehaviour : MonoBehaviour, IDamageable
 
 		if(health <= 0)
 		{
-			state = EnemyState.Dead;
+			state = State.Dead;
 			GameManager.Instance.PlayerInstance.GetComponent<PlayerBehaviour>().Mana += manaWorth;
 			anim.SetBool("Dead", true);
 			gameObject.name = gameObject.name + " -DEAD-";
@@ -193,7 +192,7 @@ public class AIBehaviour : MonoBehaviour, IDamageable
 
 	/// <summary>
 	/// IDamageable Impact Movement Speed Implementation.
-	/// This slows the enemy down to a crawl.
+	/// This slows the AI down to a crawl.
 	/// </summary>
 	/// <param name="value"></param>
 	void IDamageable.ImpactMovementSpeed(float value)
@@ -203,8 +202,8 @@ public class AIBehaviour : MonoBehaviour, IDamageable
 	}
 
 	/// <summary>
-	/// Removes all the active components once the Enemy is considered Dead.
-	/// This is done so the enemy carcass can still exist in the world without having all the code still run on the background.
+	/// Removes all the active components once the AI is considered Dead.
+	/// This is done so the AI carcass can still exist in the world without having all the code still run on the background.
 	/// </summary>
 	private void RemoveAllActiveComponents()
 	{
